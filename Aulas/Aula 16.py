2218ecc365a5689663b4a5a3ff77c2f20f3bf06c
Hamilton Costa <hamiltonvr2@gmail.com>
2024-04-22 18:31:26 -0300

    Alterando aula 72

---
+++ Aulas/Aula 16.py
@@ -0,0 +1,29 @@
+# Tuplas são imutáveis
+'''
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('=' * 40)
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+print('=' * 40)
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+print('=' * 40)
+
+print('Comi pra caramba!')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+#print(c.index(8))
+#print(c.count(5))
+#print(len(c))
+#print(sorted(c))
+'''
+pessoa = ('Gustavo', 39, 'M', 99.24)
+del(pessoa)
+print(pessoa)
